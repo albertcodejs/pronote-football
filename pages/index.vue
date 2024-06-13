81000ef7423f766559c01c24f2ec3443f4421650
Albert <albert.tumba@gmail.com>
2024-06-13 16:42:30 +0200

    refactor players composition

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,54 @@ const players = [
     position: "Gardien",
   },
   {
-    firstname: "Kylian",
-    name: "Mbappe",
-    position: "Attaquant",
+    firstname: "Jules",
+    name: "Koundé",
+    position: "Défenseur",
+  },
+  {
+    firstname: "Benjamin",
+    name: "Pavard",
+    position: "Défenseur",
+  },
+  {
+    firstname: "Jonathan",
+    name: "Clauss",
+    position: "Défenseur",
+  },
+  {
+    firstname: "Dayot",
+    name: "Upamecano",
+    position: "Défenseur",
   },
   {
     firstname: "N'Golo",
     name: "Kanté",
     position: "Milieu de terrain",
+  },
+  {
+    firstname: "Antoine",
+    name: "Griezmann",
+    position: "Milieu de terrain",
+  },
+  {
+    firstname: "Warren",
+    name: "Zaïre-Emery",
+    position: "Milieu de terrain",
+  },
+  {
+    firstname: "Kylian",
+    name: "Mbappe",
+    position: "Attaquant",
+  },
+  {
+    firstname: "Bradley",
+    name: "Barcola",
+    position: "Attaquant",
+  },
+  {
+    firstname: "Ousmane",
+    name: "Dembélé",
+    position: "Attaquant",
   },
 ];
 console.log("players", players);
