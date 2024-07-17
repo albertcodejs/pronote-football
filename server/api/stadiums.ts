b3789f9ade833e47f4da68556d1179808e983831
Albert <albert.tumba@gmail.com>
2024-07-17 18:46:41 +0200

    Add API Stadiums.ts

---
+++ server/api/stadiums.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler((event) => {
+  return [
+    {
+      name: "Düsseldorf Arena",
+      location: "Düsseldorf",
+    },
+    {
+      name: "Olympiastadion",
+      location: "Berlin",
+    },
+    {
+      name: "RheinEnergieStadion",
+      location: "Cologne",
+    },
+  ];
+});
