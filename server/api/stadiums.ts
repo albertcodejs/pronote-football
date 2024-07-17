f7790112a674e7779eb294caa4c611e5b60a3b5f
Albert <albert.tumba@gmail.com>
2024-07-17 18:53:47 +0200

    Add API Stadiums.ts 2

--- server/api/stadiums.ts
+++ server/api/stadiums.ts
@@ -12,5 +12,33 @@ export default defineEventHandler((event) => {
       name: "RheinEnergieStadion",
       location: "Cologne",
     },
+    {
+      name: "BVB Stadion Dortmund",
+      location: "Dortmund",
+    },
+    {
+      name: "Frankfurt Arena",
+      location: "Francfort-sur-le-main",
+    },
+    {
+      name: "Arena AufSchalke",
+      location: "Gelsenkirchen",
+    },
+    {
+      name: "Volksparkstadion",
+      location: "Hambourg",
+    },
+    {
+      name: "Red Bull Arena",
+      location: "Leipzig",
+    },
+    {
+      name: "",
+      location: "",
+    },
+    {
+      name: "",
+      location: "",
+    },
   ];
 });
