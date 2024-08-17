fe0584520b5a50f1638c48e2539ad4f69bd6d382
Albert <albert.tumba@gmail.com>
2024-08-17 23:26:18 +0200

    header 2

--- app.vue
+++ app.vue
@@ -5,7 +5,6 @@
         Pronote Football : Notez les joueurs de votre équipe préferée
         après-match
       </title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,9 @@
+<script setup>
+function hello() {
+  console.log("hello");
+}
+</script>
+
 <template>
   <div>
     <header class="bg-white flex flex-row justify-between pt-2 pb-2 pl-4 pr-4">
@@ -11,12 +17,15 @@
         </a>
       </div>
       <div class="content-center">
-        <i class="fa-solid fa-bars text-2xl"></i>
-        <!-- <a href="#" class="mr-1 sm:mr-4">Pronote football</a>
+        <i class="fa-solid fa-bars text-2xl" @click="hello()"></i>
+      </div>
+      <!--
+      <div class="flex content-center">
+        <a href="#" class="mr-1 sm:mr-4">Pronote football</a>
         <a href="#" class="sm:mr-4">Derniers matchs à noter</a>
         <a href="#" class="sm:mr-4">Joueurs</a>
-        <a href="#" class="sm:mr-4">Championnat</a> -->
-      </div>
+        <a href="#" class="sm:mr-4">Championnat</a>
+      </div> -->
     </header>
     <slot />
   </div>
