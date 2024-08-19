b81a04539d08ec91c87297bb26c59cc8f212e716
Albert <albert.tumba@gmail.com>
2024-08-20 01:45:10 +0200

    add cross

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 
 let mobile = true;
-let mobileNav = ref(true);
+let mobileNav = ref(false);
 </script>
 
 <template>
@@ -21,12 +21,16 @@ let mobileNav = ref(true);
       </div>
       <div class="content-center">
         <i
-          class="fa-solid fa-bars text-2xl"
-          :class="{ 'icon-active': mobileNav }"
-          v-show="mobile"
+          class="fa-solid fa-xmark text-2xl"
+          v-if="mobile && mobileNav"
           @click="mobileNav = !mobileNav"
         ></i>
-        <i class="fa-solid fa-xmark text-2xl"></i>
+        <i
+          class="fa-solid fa-bars text-2xl"
+          :class="{ 'icon-active': mobileNav }"
+          @click="mobileNav = !mobileNav"
+          v-else
+        ></i>
       </div>
       <ul
         class="flex content-center list-none no-underline text-xs"
@@ -42,17 +46,17 @@ let mobileNav = ref(true);
           v-show="mobileNav"
           class="flex flex-col fixed w-full max-w-64 h-full dropdown-nav bg-white top-0 left-0 pt-5 pb-20"
         >
-          <li class="pl-2 pb-5 uppercase font-bold">
-            <a href="#" class="mr-1 sm:mr-4 text-red-800">Pronote football</a>
+          <li class="pl-2 pb-5 font-bold">
+            <a href="#" class="mr-1 sm:mr-4 text-black">Pronote football</a>
           </li>
-          <li class="pl-2 pb-5 uppercase font-bold">
-            <a href="#" class="sm:mr-4 text-red-800">Derniers matchs à noter</a>
+          <li class="pl-2 pb-5 font-bold">
+            <a href="#" class="sm:mr-4 text-black">Derniers matchs à noter</a>
           </li>
-          <li class="pl-2 pb-5 uppercase font-bold">
-            <a href="#" class="sm:mr-4 text-red-800">Joueurs</a>
+          <li class="pl-2 pb-5 font-bold">
+            <a href="#" class="sm:mr-4 text-black">Joueurs</a>
           </li>
-          <li class="pl-2 pb-5 uppercase font-bold">
-            <a href="#" class="sm:mr-4 text-red-800">Championnat</a>
+          <li class="pl-2 pb-5 font-bold">
+            <a href="#" class="sm:mr-4 text-black">Championnat</a>
           </li>
         </ul>
       </transition>
